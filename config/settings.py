d55f56eec8636274bffce2a1af86e427a2cc3f16
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-09-14 18:10:50 +0800

    feat: initialize Django project structure

---
+++ config/settings.py
@@ -0,0 +1,104 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+BASE_DIR: Path = Path(__file__).resolve().parent.parent
+
+SECRET_KEY: Optional[str] = os.getenv("DJANGO_SECRET_KEY")
+
+DEBUG: bool = os.getenv("DJANGO_DEBUG") == "True"
+
+ALLOWED_HOSTS: List[str] = [
+    "127.0.0.1",
+]
+
+INTERNAL_IPS: List[str] = [
+    "127.0.0.1",
+]
+
+INSTALLED_APPS: List[str] = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE: List[str] = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF: str = "config.urls"
+
+TEMPLATES: List[Dict[str, Any]] = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION: str = "config.wsgi.application"
+
+DATABASES: Dict[str, Any] = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS: List[Dict[str, str]] = [
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE: str = "ru-RU"
+
+TIME_ZONE: str = "Europe/Moscow"
+
+USE_I18N: bool = True
+
+USE_TZ: bool = True
+
+STATIC_URL: str = "static/"
+STATICFILES_DIRS: List[Any] = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT: Path = BASE_DIR / "staticfiles"
+
+MEDIA_URL: str = "media/"
+MEDIA_ROOT: Path = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
+
+FIXTURE_DIRS: List[str] = [
+    "fixtures",
+]
