084ab28ed3bcfca9f0ba8a7f36008fda013fb363
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-10-12 02:37:08 +0800

    feat(catalog): add urls.py and include catalog routes in main urls

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,15 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import URLPattern, URLResolver, path
+from django.urls import URLPattern, URLResolver, include, path
 
 urlpatterns: List[Union[URLResolver, URLPattern]] = [
     path("admin/", admin.site.urls),
+    path(
+        "catalog/",
+        include("apps.catalog.urls", namespace="catalog"),
+        name="catalog",
+    ),
 ]
 
 
