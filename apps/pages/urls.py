e5c6d4ff76f38e4cf25fe465c3d8509058e81392
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-10-23 19:30:14 +0800

    feat(pages): implement legal views (privacy, terms, offer), templates and styles

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -7,5 +7,8 @@ from . import views
 app_name: str = "pages"
 
 urlpatterns: List[URLPattern] = [
-    path("contact", views.ContactView.as_view(), name="contact")
+    path("contact", views.ContactView.as_view(), name="contact"),
+    path("privacy", views.PrivacyView.as_view(), name="privacy"),
+    path("terms", views.TermsView.as_view(), name="terms"),
+    path("offer", views.OfferView.as_view(), name="offer"),
 ]
