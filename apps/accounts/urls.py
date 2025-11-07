bfb0f3719ebda6ff81801cd58d50d2424f8e1984
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-11-08 00:13:13 +0800

    feat(accounts): implement sign up view, template and styles

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -9,4 +9,5 @@ app_name: str = "accounts"
 urlpatterns: List[URLPattern] = [
     path("signin", views.SignInView.as_view(), name="signin"),
     path("signout", views.SignOutView.as_view(), name="signout"),
+    path("signup", views.SignUpView.as_view(), name="signup"),
 ]
