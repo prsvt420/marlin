2fb61f371664f6427b7e9597b33a0cff5f29b755
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-11-11 01:40:20 +0800

    feat(catalog): add translations to app config and unit type choices

--- apps/catalog/apps.py
+++ apps/catalog/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CatalogConfig(AppConfig):
@@ -9,5 +10,5 @@ class CatalogConfig(AppConfig):
 
     default_auto_field: str = "django.db.models.BigAutoField"
     name: str = "apps.catalog"
-    verbose_name: str = "Каталог"
+    verbose_name = _("Catalog")
     label: str = "catalog"
