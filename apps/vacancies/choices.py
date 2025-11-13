3bf483a4ab5c8c726624791d8872a2523298376f
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-11-13 19:32:28 +0800

    feat(vacancies): add translations to app config and experience/schedule choices

--- apps/vacancies/choices.py
+++ apps/vacancies/choices.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class ExperienceLevel(models.TextChoices):
@@ -14,10 +15,10 @@ class ExperienceLevel(models.TextChoices):
         MORE_THAN_SIX (str): More than 6 years of experience.
     """
 
-    NO_EXPERIENCE = "no_exp", "Без опыта"
-    ONE_TO_THREE = "1_3", "От 1 года до 3 лет"
-    THREE_TO_SIX = "3_6", "От 3 до 6 лет"
-    MORE_THAN_SIX = "6_plus", "Более 6 лет"
+    NO_EXPERIENCE = "no_exp", _("No experience")
+    ONE_TO_THREE = "1_3", _("From 1 to 3 years")
+    THREE_TO_SIX = "3_6", _("From 3 to 6 years")
+    MORE_THAN_SIX = "6_plus", _("More than 6 years")
 
 
 class WorkSchedule(models.TextChoices):
@@ -33,7 +34,7 @@ class WorkSchedule(models.TextChoices):
         REMOTE (str): Remote work.
     """
 
-    FULL_TIME = "full", "Полный день"
-    SHIFT = "shift", "Сменный график"
-    FLEXIBLE = "flex", "Гибкий график"
-    REMOTE = "remote", "Удаленная работа"
+    FULL_TIME = "full", _("Full-time")
+    SHIFT = "shift", _("Shift schedule")
+    FLEXIBLE = "flex", _("Flexible schedule")
+    REMOTE = "remote", _("Remote work")
