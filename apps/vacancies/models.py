1c6a90127bf3abb6614843847e03400fa7a83d5a
Tokarev Denis <wf.denis.tokarev@gmail.com>
2025-11-14 05:11:58 +0800

    feat(vacancies): add translations to region model

--- apps/vacancies/models.py
+++ apps/vacancies/models.py
@@ -258,15 +258,15 @@ class Region(models.Model):
     name = models.CharField(
         max_length=255,
         unique=True,
-        verbose_name="название",
-        help_text="Название региона.",
+        verbose_name=_("name"),
+        help_text=_("Region name."),
     )
 
     class Meta:  # noqa: D106
         db_table = "vacancies_region"
-        db_table_comment = "Таблица с информацией о регионах"
-        verbose_name = "регион"
-        verbose_name_plural = "регионы"
+        db_table_comment = "Table containing regions."
+        verbose_name = _("region")
+        verbose_name_plural = _("regions")
         ordering = ("name",)
 
     def __str__(self) -> str:
