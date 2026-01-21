1bfb6fb69e93fad8b6871501eb7dbb665fe64900
Tokarev Denis <wf.denis.tokarev@gmail.com>
2026-01-21 15:03:21 +0800

    refactor(promotions): convert relative imports to absolute

--- apps/promotions/urls.py
+++ apps/promotions/urls.py
@@ -2,7 +2,7 @@ from typing import List
 
 from django.urls import URLPattern, path  # noqa: F401
 
-from . import views  # noqa: F401
+from apps.promotions import views  # noqa: F401
 
 app_name: str = "promotions"
 
