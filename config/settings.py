0639cf61db12b1cc7d200fd78c9a547483fa8788
Tokarev Denis <wf.denis.tokarev@gmail.com>
2026-02-18 01:14:37 +0800

    build: add Redis cache

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,13 @@ DATABASES: Dict[str, Any] = {
     }
 }
 
+CACHES: Dict[str, Any] = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": config("CACHE_LOCATION", default="redis://127.0.0.1:6379"),
+    }
+}
+
 AUTH_PASSWORD_VALIDATORS: List[Dict[str, str]] = [
     {
         "NAME": "django.contrib.auth.password_validation."
