26f672e5e03878c160bd21896733651a7753082f
Tokarev Denis <wf.denis.tokarev@gmail.com>
2026-02-18 00:58:39 +0800

    build: replace SQLite with PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION: str = "config.wsgi.application"
 
 DATABASES: Dict[str, Any] = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DATABASE_NAME"),
+        "USER": config("DATABASE_USER"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
+        "HOST": config("DATABASE_HOST", default="127.0.0.1"),
+        "PORT": config("DATABASE_PORT", default=5432),
     }
 }
 
