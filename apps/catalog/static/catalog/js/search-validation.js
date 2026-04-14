1e5a37a7b540ffae93f4a7698490891cb428ecb4
Tokarev Denis <wf.denis.tokarev@gmail.com>
2026-04-15 03:39:13 +0800

    feat: update form, input and link design

--- apps/catalog/static/catalog/js/search-validation.js
+++ apps/catalog/static/catalog/js/search-validation.js
@@ -2,12 +2,7 @@ const searchInput = document.getElementById("search-input");
 const searchForm = searchInput.closest("form");
 
 searchForm.addEventListener("submit", (e) => {
-  if (searchInput.value.trim().length < 2) {
+  if (!searchInput.value.trim().length) {
     e.preventDefault();
-    searchInput.classList.add("border", "border-error");
   }
-});
-
-searchInput.addEventListener("blur", () => {
-  searchInput.classList.remove("border", "border-error");
 });
